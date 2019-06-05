7edd63089340ed52b437a15282f21d873a9d632e
Nguyễn Hoàng Nhật Phi <nguyenhoangnhatphi@gmail.com>
2019-06-05 11:00:51 +0700

    table categories

--- src/screens/StoreInfoScreen/widget/TabServiceProduct/widget/TabCategories.js
+++ src/screens/StoreInfoScreen/widget/TabServiceProduct/widget/TabCategories.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import {
     View,
-    Image,
-    Text,
+    FlatList,
     StyleSheet
 } from 'react-native';
 
 import { FooterTab } from '../../../../../components';
 import { scaleSzie } from '../../../../../utils';
+import TableHeaderCategories from './TableHeaderCategories';
+import RowTableCategories from './RowTableCategories';
 import IMAGE from '../../../../../resources';
+
+const FakeData = [{
+    id: 'HP000002',
+    name: 'Pena Valdez',
+    role: 'Staff',
+    status: 'Active'
+},
+{
+    id: 'HP000003',
+    name: 'Jessica Miles',
+    role: 'Staff',
+    status: 'Active'
+},
+{
+    id: 'HP000004',
+    name: 'Pena Valdez',
+    role: 'Staff',
+    status: 'Active'
+}, {
+    id: 'HP000005',
+    name: 'Jessica Miles',
+    role: 'Staff',
+    status: 'Active'
+}]
+
 
 class TabCategories extends React.Component {
 
@@ -16,11 +42,34 @@ class TabCategories extends React.Component {
         super(props);
     }
 
-    
+
+    renderTable() {
+        return (
+            <View style={{ flex: 1 }} >
+                <TableHeaderCategories />
+                <View style={{ flex: 1 }} >
+                    <FlatList
+                        data={FakeData}
+                        renderItem={({ item, index }) => <RowTableCategories
+                            // ref={this.setRefStaff}
+                            key={index}
+                            key={index} index={parseInt(index + 1)}
+                            staff={item}
+                            archiveStaff={() => this.togglePopupArchive(true, item)}
+                            restoreStaff={() => this.togglePopupRestore(true, item)}
+                            editStaff={() => this.editStaff()}
+                        />}
+                        keyExtractor={(item, index) => item.id}
+                    />
+                </View>
+            </View>
+        );
+    }
 
     render() {
         return (
             <View style={styles.container} >
+                {this.renderTable()}
                 <FooterTab />
             </View>
 
@@ -31,7 +80,7 @@ class TabCategories extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop:scaleSzie(15)
+        paddingTop: scaleSzie(12)
     },
 })
 
