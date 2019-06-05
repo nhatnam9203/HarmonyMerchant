209c2bcd0abacfdb834eb4ecb053939ac2636447
Nguyễn Hoàng Nhật Phi <nguyenhoangnhatphi@gmail.com>
2019-06-05 14:27:18 +0700

    init table services

---
+++ src/screens/StoreInfoScreen/widget/TabServiceProduct/widget/RowTableServices.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import {
+    View,
+    Image,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+import { ButtonCustom, } from '../../../../../components';
+import { scaleSzie } from '../../../../../utils';
+import IMAGE from '../../../../../resources';
+
+class RowTableServices extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isArchive: true
+        }
+    }
+
+    handleArchirveStaff = () => {
+        this.setState({
+            isArchive: false
+        })
+    }
+
+    handleRestoreStaff = () => {
+        this.setState({
+            isArchive: true
+        })
+    }
+
+    render() {
+        const { staff, index, archiveService, editService, restoreService } = this.props;
+        const { isArchive } = this.state;
+        return (
+            <View style={styles.tableHeader} >
+                {/* ----- 1 ------ */}
+                <View style={{
+                    width: scaleSzie(230), flexDirection: 'row',
+                }} >
+                    <View style={[{
+                        flex: 1,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        paddingLeft: scaleSzie(12)
+                    }]} >
+                        <Image source={IMAGE.indicate}
+                            style={{ width: scaleSzie(12), height: scaleSzie(29), marginRight: scaleSzie(12) }}
+                        />
+                        <Text style={styles.textTableHeader} >
+                            {staff.name}
+                        </Text>
+                    </View>
+                    <View style={{ width: 1, paddingVertical: scaleSzie(3) }} >
+                        <View style={{ flex: 1, backgroundColor: '#E5E5E5' }} />
+                    </View>
+                </View>
+                {/* ----- 2 ----- */}
+                <View style={{
+                    width: scaleSzie(180), flexDirection: 'row',
+                }} >
+                    <View style={{ flex: 1, justifyContent: 'center', paddingLeft: scaleSzie(10) }} >
+                        <Text style={styles.textTableHeader} >
+                            {staff.status}
+                        </Text>
+                    </View>
+                    <View style={{ width: 1, paddingVertical: scaleSzie(3) }} >
+                        <View style={{ flex: 1, backgroundColor: '#E5E5E5' }} />
+                    </View>
+                </View>
+                {/* ----- 3 ----- */}
+                <View style={{
+                    width: scaleSzie(180), flexDirection: 'row',
+                }} >
+                    <View style={{ flex: 1, justifyContent: 'center', paddingLeft: scaleSzie(10) }} >
+                        <Text style={styles.textTableHeader} >
+                            {staff.status}
+                        </Text>
+                    </View>
+                    <View style={{ width: 1, paddingVertical: scaleSzie(3) }} >
+                        <View style={{ flex: 1, backgroundColor: '#E5E5E5' }} />
+                    </View>
+                </View>
+                {/* ----- 4 ------ */}
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'row'
+                }} >
+                    <View style={styles.actionButton} >
+                        <ButtonCustom
+                            width={'80%'}
+                            height={28}
+                            backgroundColor="#0764B0"
+                            title="Edit"
+                            textColor="#fff"
+                            onPress={() => editService()}
+                            style={{ borderWidth: 1, borderColor: '#C5C5C5', borderRadius: scaleSzie(2) }}
+                            styleText={{
+                                fontSize: scaleSzie(14)
+                            }}
+                        />
+                    </View>
+                    <View style={styles.actionButton} >
+                        {
+                            isArchive ? <ButtonCustom
+                                width={'80%'}
+                                height={28}
+                                backgroundColor="#FF3B30"
+                                title="Archive"
+                                textColor="#fff"
+                                onPress={() => archiveService()}
+                                style={{ borderWidth: 1, borderColor: '#C5C5C5', borderRadius: scaleSzie(2) }}
+                                styleText={{
+                                    fontSize: scaleSzie(14)
+                                }}
+                            /> :
+                                <ButtonCustom
+                                    width={'80%'}
+                                    height={28}
+                                    backgroundColor="#F1F1F1"
+                                    title="Restore"
+                                    textColor="#6A6A6A"
+                                    onPress={() => restoreService()}
+                                    style={{ borderWidth: 1, borderColor: '#C5C5C5', borderRadius: scaleSzie(2) }}
+                                    styleText={{
+                                        fontSize: scaleSzie(14)
+                                    }}
+                                />
+                        }
+
+                    </View>
+                </View>
+            </View>
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    tableHeader: {
+        height: scaleSzie(60),
+        backgroundColor: '#FAFAFA',
+        borderWidth: 0.5,
+        borderColor: '#C5C5C5',
+        flexDirection: 'row'
+    },
+    textTableHeader: {
+        color: '#6A6A6A',
+        fontSize: scaleSzie(14)
+    },
+    itemTableHeaderContainer: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    actionButton: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
+})
+
+export default RowTableServices;
+
--- src/screens/StoreInfoScreen/widget/TabServiceProduct/widget/TabServices.js
+++ src/screens/StoreInfoScreen/widget/TabServiceProduct/widget/TabServices.js
@@ -1,24 +1,179 @@
 import React from 'react';
 import {
     View,
-    Image,
-    Text,
+    FlatList,
     StyleSheet
 } from 'react-native';
 
-import { InputAuth, ButtonCustom, Button, DefaultTabBar } from '../../../../../components';
+import { FooterTab, PopupConfirm } from '../../../../../components';
 import { scaleSzie } from '../../../../../utils';
+import HeaderTableServices from './HeaderTableServices';
+import RowTableServices from './RowTableServices';
+import IMAGE from '../../../../../resources';
+import PopupEditAddService from './PopupEditAddService';
+
+const FakeData = [{
+    id: 'HP000002',
+    name: 'Pena Valdez',
+    role: 'Staff',
+    status: 'Active'
+},
+{
+    id: 'HP000003',
+    name: 'Jessica Miles',
+    role: 'Staff',
+    status: 'Active'
+},
+{
+    id: 'HP000004',
+    name: 'Pena Valdez',
+    role: 'Staff',
+    status: 'Active'
+}, {
+    id: 'HP000005',
+    name: 'Jessica Miles',
+    role: 'Staff',
+    status: 'Active'
+}]
+
 
 class TabServices extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = {
+            visibleArchive: false,
+            visibleRestore: false,
+            visibleAdd:false,
+            visibleEdit:false,
+            serviceInfoHandle: {}
+        }
+
+        this.inputRefsService = [];
+    }
+
+    setRefService = (ref) => {
+        if (ref != null) {
+            this.inputRefsService.push(ref);
+        }
+    };
+
+    async  togglePopupArchive(bool, service = {}) {
+        if (bool === true) {
+            await this.setState({
+                serviceInfoHandle: service
+            })
+        }
+        this.setState(prevState => ({
+            visibleArchive: bool
+        }))
+    }
+
+    async  togglePopupRestore(bool, service = {}) {
+        if (bool === true) {
+            await this.setState({
+                serviceInfoHandle: service
+            })
+        }
+        this.setState(prevState => ({
+            visibleRestore: bool
+        }))
+    }
+
+    archirveServiceYess() {
+        const { serviceInfoHandle } = this.state;
+        for (let i = 0; i < this.inputRefsService.length; i++) {
+            if (this.inputRefsService[i].props.staff.id === serviceInfoHandle.id) {
+                this.inputRefsService[i].handleArchirveStaff();
+                break;
+            }
+        }
+        this.setState({
+            visibleArchive: false
+        })
+    }
+
+    restoreStaffYess() {
+        const { serviceInfoHandle } = this.state;
+        for (let i = 0; i < this.inputRefsService.length; i++) {
+            if (this.inputRefsService[i].props.staff.id === serviceInfoHandle.id) {
+                this.inputRefsService[i].handleRestoreStaff();
+                break;
+            }
+        }
+        this.setState({
+            visibleRestore: false
+        })
+    }
+
+   async editService(service){
+      await  this.setState({
+            serviceInfoHandle:service
+        });
+        this.setState({
+            visibleEdit:true
+        })
+    }
+
+    renderTable() {
+        return (
+            <View style={{ flex: 1 }} >
+                <HeaderTableServices />
+                <View style={{ flex: 1 }} >
+                    <FlatList
+                        data={FakeData}
+                        renderItem={({ item, index }) => <RowTableServices
+                            ref={this.setRefService}
+                            key={index}
+                            key={index} index={parseInt(index + 1)}
+                            staff={item}
+                            archiveService={() => this.togglePopupArchive(true, item)}
+                            restoreService={() => this.togglePopupRestore(true, item)}
+                            editService={() => this.editService(item)}
+                        />}
+                        keyExtractor={(item, index) => item.id}
+                    />
+                </View>
+            </View>
+        );
     }
 
     render() {
+        const { visibleArchive, visibleRestore ,visibleAdd,visibleEdit} = this.state;
         return (
             <View style={styles.container} >
-               
+                {this.renderTable()}
+                <FooterTab 
+                addNew={() => this.setState({visibleAdd:true})}
+                />
+                <PopupConfirm
+                    visible={visibleArchive}
+                    title="Confirmation"
+                    message="Do you want to Archive this Category ?"
+                    onRequestClose={() => this.togglePopupArchive(false)}
+                    confimYes={() => this.archirveServiceYess()}
+                />
+                <PopupConfirm
+                    visible={visibleRestore}
+                    title="Confirmation"
+                    message="Do you want to Restore this Category ?"
+                    onRequestClose={() => this.togglePopupRestore(false)}
+                    confimYes={() => this.restoreStaffYess()}
+                />
+                <PopupEditAddService
+                    visible={visibleAdd}
+                    title="Add Category"
+                    titleButton="Add"
+                    onRequestClose={() => this.setState({visibleAdd:false})}
+                    confimYes={() => this.setState({visibleAdd:false})}
+                />
+                <PopupEditAddService
+                    visible={visibleEdit}
+                    title="Edit Category"
+                    titleButton="Save"
+                    onRequestClose={() => this.setState({visibleEdit:false})}
+                    confimYes={() => this.setState({visibleEdit:false})}
+                />
             </View>
 
         );
@@ -28,6 +183,7 @@ class TabServices extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingTop: scaleSzie(12)
     },
 })
 
