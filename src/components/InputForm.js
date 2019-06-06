0279a765ef49fe4342d53abf130bb1750e816d7b
Nguyễn Hoàng Nhật Phi <nguyenhoangnhatphi@gmail.com>
2019-06-06 14:52:37 +0700

    general info

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -11,27 +11,27 @@ import IMAGE from '../resources';
 
 export default class InputForm extends React.PureComponent {
     render() {
-        const {title,subTitle,placeholder} = this.props;
+        const { title, subTitle, placeholder ,style} = this.props;
         return (
-            <View style={{marginBottom:scaleSzie(24)}} >
-            <Text style={{ color: '#404040', fontSize: scaleSzie(14) }} >
-                {title}
-                <Text style={{ color: '#404040', fontSize: scaleSzie(12) }} >
-                    
-                    {subTitle}
-                </Text>
-            </Text>
-            <View style={{
-                height: scaleSzie(38), borderWidth: 1, borderColor: '#6A6A6A',
-                marginTop: scaleSzie(5), paddingLeft: scaleSzie(8)
-            }} >
-                <TextInput
-                    style={{ flex: 1, fontSize: scaleSzie(16) }}
-                    placeholder={placeholder}
-                />
+            <View style={[{ marginBottom: scaleSzie(24) },style]} >
+                <Text style={{ color: '#404040', fontSize: scaleSzie(14) }} >
+                    {title}
+                    <Text style={{ color: '#404040', fontSize: scaleSzie(12) }} >
 
+                        {subTitle}
+                    </Text>
+                </Text>
+                <View style={{
+                    height: scaleSzie(30), borderWidth: 1, borderColor: '#6A6A6A',
+                    marginTop: scaleSzie(5), paddingLeft: scaleSzie(8)
+                }} >
+                    <TextInput
+                        style={{ flex: 1, fontSize: scaleSzie(16) }}
+                        placeholder={placeholder}
+                    />
+
+                </View>
             </View>
-        </View>
         );
     }
 
--- src/screens/GeneralInfoScreen/layout.js
+++ src/screens/GeneralInfoScreen/layout.js
@@ -6,11 +6,20 @@ import {
     TextInput
 } from 'react-native';
 
-import { InputForm, FormInfoParent,Text } from '../../components';
+import { InputForm, FormInfoParent, Text, Dropdown } from '../../components';
 import { scaleSzie } from '../../utils';
 import styles from './style';
 import Configs from '../../configs';
 import IMAGE from '../../resources';
+
+let data = [{
+    value: 'Banana',
+}, {
+    value: 'Mango',
+}, {
+    value: 'Pear',
+}
+];
 
 export default class Layout extends React.Component {
 
@@ -31,20 +40,24 @@ export default class Layout extends React.Component {
                                 subTitle="(as shown on your income tax return)"
                                 placeholder="Legal Business Name"
                             />
-                            <View style={{ flexDirection: 'row' }} >
-                                <View style={{width:scaleSzie(100)}} >
+                            <InputForm
+                                title="Doing Business As Name (DBA) *"
+                                subTitle=""
+                                placeholder="DBA"
+                            /><View style={{ flexDirection: 'row' }} >
+                                <View style={{ width: scaleSzie(100) }} >
                                     <InputForm
                                         title="Federal Tax ID *"
                                         subTitle=""
                                         placeholder=""
                                     />
                                 </View>
-                                <View style={{width:scaleSzie(20),justifyContent:'center',alignItems:'center'}} >
-                                    <Text style={{fontSize :scaleSzie(20)}} >
+                                <View style={{ width: scaleSzie(20), justifyContent: 'center', alignItems: 'center' }} >
+                                    <Text style={{ fontSize: scaleSzie(20) }} >
                                         -
-                                    </Text>
+                                </Text>
                                 </View>
-                                <View style={{ width:scaleSzie(250)}} >
+                                <View style={{ width: scaleSzie(250) }} >
                                     <InputForm
                                         title="   "
                                         subTitle=""
@@ -53,16 +66,53 @@ export default class Layout extends React.Component {
                                 </View>
                             </View>
                             <InputForm
-                                title="Doing Business As Name (DBA) *"
-                                subTitle=""
-                                placeholder="DBA"
-                            />
-                            <InputForm
                                 title="DBA Business Address *"
                                 subTitle="(no P.O. Box)"
-                                placeholder="DBA"
+                                placeholder="DBA address"
+                                style={{
+                                    marginBottom: scaleSzie(10)
+                                }}
                             />
-                            <View style={{ flexDirection: 'row' }} >
+                            <View style={{
+                                height: scaleSzie(30), marginBottom: scaleSzie(24), justifyContent: 'space-between',
+                                flexDirection: 'row',alignItems:'flex-end'
+                            }} >
+                                <View style={{ width: scaleSzie(180) }} >
+                                    <InputForm
+                                        title=""
+                                        subTitle=""
+                                        placeholder="City"
+                                        style={{
+                                            marginBottom: 0
+                                        }}
+                                    />
+                                </View>
+                                <View style={{ width: scaleSzie(180),backgroundColor:'red' }} >
+                                    <Dropdown
+                                        label={'State'}
+                                        data={data}
+                                        // value={timeStart}
+                                        // onChangeText={(value) => this.setState({ timeStart: value })}
+                                        containerStyle={{
+                                            backgroundColor: '#F1F1F1',
+                                            borderWidth: 1,
+                                            borderColor: '#6A6A6A',
+                                            flex: 1
+                                        }}
+                                    />
+                                </View>
+                                <View style={{ width: scaleSzie(180) }} >
+                                    <InputForm
+                                        title=""
+                                        subTitle=""
+                                        placeholder="Zip"
+                                        style={{
+                                            marginBottom: 0
+                                        }}
+                                    />
+                                </View>
+                            </View>
+                            <View style={{ flexDirection: 'row', marginTop: scaleSzie(10) }} >
                                 <View style={{ flex: 1 }} >
                                     <InputForm
                                         title="Business Phone Number *"
