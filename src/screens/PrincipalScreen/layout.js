053feeb8d95de2e103d2c6906116465a06f31b2f
Nguyễn Hoàng Nhật Phi <nguyenhoangnhatphi@gmail.com>
2019-06-06 22:53:52 +0700

    Pricipal info

--- src/screens/PrincipalScreen/layout.js
+++ src/screens/PrincipalScreen/layout.js
@@ -44,40 +44,62 @@ export default class Layout extends React.Component {
                             showsVerticalScrollIndicator={false}
                         >
                             <View style={{ height: scaleSzie(16) }} />
+                            <Text style={{
+                                color: '#404040', fontSize: scaleSzie(18), fontWeight: 'bold',
+                                marginBottom: scaleSzie(10)
+                            }} >
+                                Principal 1 *
+                            </Text>
+
+                            <View style={{ flexDirection: 'row' }} >
+                                <View style={{ flex: 1 }} >
+                                    <InputForm
+                                        title="Principal Name *"
+                                        subTitle=""
+                                        placeholder="First name"
+                                    />
+                                </View>
+                                <View style={{ flex: 1, paddingLeft: scaleSzie(20) }} >
+                                    <InputForm
+                                        title="   "
+                                        subTitle=""
+                                        placeholder="Last name"
+                                    />
+                                </View>
+                            </View>
+
                             <InputForm
-                                title="Legal Business Name * "
-                                subTitle="(as shown on your income tax return)"
-                                placeholder="Legal Business Name"
+                                title="Title/Position *"
+                                subTitle=""
+                                placeholder="Manager"
                             />
                             <InputForm
-                                title="Doing Business As Name (DBA) *"
+                                title="Ownership (%) *"
                                 subTitle=""
-                                placeholder="DBA"
-                            /><View style={{ flexDirection: 'row' }} >
-                                <View style={{ width: scaleSzie(100) }} >
+                                placeholder=""
+                            />
+
+                            <View style={{ flexDirection: 'row' }} >
+                                <View style={{ flex: 1 }} >
                                     <InputForm
-                                        title="Federal Tax ID *"
+                                        title="Home Phone *"
                                         subTitle=""
                                         placeholder=""
                                     />
                                 </View>
-                                <View style={{ width: scaleSzie(20), justifyContent: 'center', alignItems: 'center' }} >
-                                    <Text style={{ fontSize: scaleSzie(20) }} >
-                                        -
-                                </Text>
-                                </View>
-                                <View style={{ width: scaleSzie(250) }} >
+                                <View style={{ flex: 1, paddingLeft: scaleSzie(20) }} >
                                     <InputForm
-                                        title="   "
+                                        title="Mobile Phone"
                                         subTitle=""
                                         placeholder=""
                                     />
                                 </View>
                             </View>
+
                             <InputForm
-                                title="DBA Business Address *"
-                                subTitle="(no P.O. Box)"
-                                placeholder="DBA address"
+                                title="Address *"
+                                subTitle=""
+                                placeholder="Home address"
                                 style={{
                                     marginBottom: scaleSzie(10)
                                 }}
@@ -121,43 +143,74 @@ export default class Layout extends React.Component {
                                     />
                                 </View>
                             </View>
-                            <View style={{ flexDirection: 'row', marginTop: scaleSzie(10) }} >
-                                <View style={{ flex: 1 }} >
-                                    <InputForm
-                                        title="Business Phone Number *"
-                                        subTitle=""
-                                        placeholder=""
-                                    />
-                                </View>
-                                <View style={{ flex: 1 }} />
-                            </View>
 
                             <InputForm
-                                title="Contact's  Email Address *"
+                                title="Years at This Address *"
+                                subTitle=""
+                                placeholder=""
+                            />
+                            <InputForm
+                                title="Social Security Number (SSN) *"
+                                subTitle=""
+                                placeholder=""
+                            />
+                            {/* ------ thieu ----- */}
+                            <Text style={{ color: '#404040', fontSize: scaleSzie(14), marginBottom: scaleSzie(6) }} >
+                                Date of Birth (dd/mm/yy) *
+                            </Text>
+                            <View style={{
+                                height: scaleSzie(30), marginBottom: scaleSzie(24),
+                                flexDirection: 'row', alignItems: 'flex-end'
+                            }} >
+                                <View style={{ width: scaleSzie(120) }} >
+                                    <Dropdown
+                                        label={'1'}
+                                        data={data}
+                                        // value={timeStart}
+                                        // onChangeText={(value) => this.setState({ timeStart: value })}
+                                        containerStyle={{
+                                            backgroundColor: '#F1F1F1',
+                                            borderWidth: 1,
+                                            borderColor: '#6A6A6A',
+                                            flex: 1
+                                        }}
+                                    />
+                                </View>
+                                <View style={{ width: scaleSzie(120), marginHorizontal: scaleSzie(20) }} >
+                                    <Dropdown
+                                        label={'Jan'}
+                                        data={data}
+                                        // value={timeStart}
+                                        // onChangeText={(value) => this.setState({ timeStart: value })}
+                                        containerStyle={{
+                                            backgroundColor: '#F1F1F1',
+                                            borderWidth: 1,
+                                            borderColor: '#6A6A6A',
+                                            flex: 1
+                                        }}
+                                    />
+                                </View>
+                                <View style={{ width: scaleSzie(120) }} >
+                                    <Dropdown
+                                        label={'2000'}
+                                        data={data}
+                                        // value={timeStart}
+                                        // onChangeText={(value) => this.setState({ timeStart: value })}
+                                        containerStyle={{
+                                            backgroundColor: '#F1F1F1',
+                                            borderWidth: 1,
+                                            borderColor: '#6A6A6A',
+                                            flex: 1
+                                        }}
+                                    />
+                                </View>
+                            </View>
+
+                            {/* ------------- */}
+                            <InputForm
+                                title="Email Address *"
                                 subTitle=""
                                 placeholder="example@gmail.com"
-                            />
-
-                            <View style={{ flexDirection: 'row' }} >
-                                <View style={{ flex: 1 }} >
-                                    <InputForm
-                                        title="Contact Name *"
-                                        subTitle=""
-                                        placeholder="First name"
-                                    />
-                                </View>
-                                <View style={{ flex: 1, paddingLeft: scaleSzie(20) }} >
-                                    <InputForm
-                                        title="   "
-                                        subTitle=""
-                                        placeholder="Last name"
-                                    />
-                                </View>
-                            </View>
-                            <InputForm
-                                title="Title/Position *"
-                                subTitle=""
-                                placeholder="Manager"
                             />
 
                             <View style={{ flexDirection: 'row' }} >
@@ -170,6 +223,84 @@ export default class Layout extends React.Component {
                                 </View>
                                 <View style={{ flex: 1 }} />
                             </View>
+
+                            <View style={{ flexDirection: 'row' }} >
+                                <View style={{ flex: 1, paddingRight: scaleSzie(20) }} >
+                                    <InputForm
+                                        title="Principal Name *"
+                                        subTitle=""
+                                        placeholder="First name"
+                                    />
+                                </View>
+                                <View style={{ width: scaleSzie(180), }} >
+                                    <Text style={{ color: '#404040', fontSize: scaleSzie(14), marginBottom: scaleSzie(5) }} >
+                                        State Issued *
+                            </Text>
+                                    <Dropdown
+                                        label={'2000'}
+                                        data={data}
+                                        // value={timeStart}
+                                        // onChangeText={(value) => this.setState({ timeStart: value })}
+                                        containerStyle={{
+                                            backgroundColor: '#F1F1F1',
+                                            borderWidth: 1,
+                                            borderColor: '#6A6A6A',
+                                            width: scaleSzie(180),
+                                            height: scaleSzie(30)
+                                        }}
+                                    />
+                                </View>
+                            </View>
+
+                            {/* ------ Take Photo ---- */}
+
+                            <Text style={{ color: '#404040', fontSize: scaleSzie(14), marginTop: scaleSzie(10) }} >
+                                Please take or upload photos of Driver License
+                            </Text>
+
+                            <View style={{
+                                alignItems: 'center',
+                                padding: scaleSzie(10), marginTop: scaleSzie(18)
+                            }} >
+                                <View style={{
+                                    width: scaleSzie(400), height: scaleSzie(200),
+                                    borderWidth: 2, borderColor: '#C5C5C5', borderStyle: "dashed",
+                                    borderRadius: scaleSzie(14),
+                                    alignItems: 'center',
+                                    paddingTop: scaleSzie(5)
+
+                                }} >
+                                    <Image
+                                        source={IMAGE.camera}
+                                    />
+                                    <View style={{ flex: 1, justifyContent: 'space-around', alignItems: 'center' }} >
+                                        <Text style={{
+                                            color: '#6A6A6A', fontSize: scaleSzie(20), fontWeight: 'bold',
+                                        }} >
+                                            Take a Photo
+                                    </Text>
+
+                                        <Text style={{
+                                            color: '#6A6A6A', fontSize: scaleSzie(20),
+                                        }} >
+                                            Or
+                                    </Text>
+                                        <View style={{
+                                            width: scaleSzie(180), height: scaleSzie(40), backgroundColor: '#F1F1F1',
+                                            borderWidth: 1, borderColor: '#C5C5C5', borderRadius: 4, justifyContent: "center", alignItems: 'center'
+                                        }} >
+                                            <Text style={{
+                                                color: '#6A6A6A', fontSize: scaleSzie(20),
+                                            }} >
+                                                Browse File
+                                            </Text>
+                                        </View>
+                                    </View>
+
+                                </View>
+                            </View>
+
+                            {/* ---------------------- */}
 
                             <View style={{ height: scaleSzie(250) }} />
                         </ScrollView>
