16c9074e875d167c123f1c65134b40453fefbf9f
Nguyễn Hoàng Nhật Phi <nguyenhoangnhatphi@gmail.com>
2019-07-08 10:11:47 +0700

    UI check out

--- src/screens/HomeScreen/widget/TabCheckout/widget/ItemBasket.js
+++ src/screens/HomeScreen/widget/TabCheckout/widget/ItemBasket.js
@@ -10,13 +10,28 @@ const ItemBasket = ({ }) => {
     return (
         <View style={{
             height: scaleSzie(40), backgroundColor: '#0764B0', borderBottomColor: '#fff', borderBottomWidth: 2,
-            flexDirection: 'row',alignItems:'center'
+            flexDirection: 'row'
         }} >
             {/* -------- Avatar ------- */}
-            <View style={{width:scaleSzie(45),height:scaleSzie(35),backgroundColor:'red'}} >
+            <View style={{ width: scaleSzie(45),justifyContent:'center',alignItems:'center'}} >
+                <View style={{width:scaleSzie(35),height:scaleSzie(35),backgroundColor:'red',borderRadius:scaleSzie(20)}} >
 
+                </View>
             </View>
-             {/* -------- Avatar ------- */}
+            {/* -------- Avatar ------- */}
+            <View style={{ flex: 1, flexDirection: 'row' ,justifyContent:'space-between',
+        alignItems:'center',paddingHorizontal:scaleSzie(12)
+        }} >
+                <Text style={{ color: '#fff', fontSize: scaleSzie(13) }} >
+                    Meredith
+                </Text>
+                <Text style={{ color: '#fff', fontSize: scaleSzie(13) }} >
+                    Service 1
+                </Text>
+                <Text style={{ color: '#fff', fontSize: scaleSzie(14) }} >
+                $ 100
+                </Text>
+            </View>
         </View>
     );
 }
