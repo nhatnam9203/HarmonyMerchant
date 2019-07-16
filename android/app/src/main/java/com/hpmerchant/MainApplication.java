b8de92eada1228211961d5d9d325c3db9b41620d
Nguyễn Hoàng Nhật Phi <nguyenhoangnhatphi@gmail.com>
2019-07-16 10:35:46 +0700

    setup react-native-star-prnt

--- android/app/src/main/java/com/hpmerchant/MainApplication.java
+++ android/app/src/main/java/com/hpmerchant/MainApplication.java
@@ -3,6 +3,7 @@ package com.hpmerchant;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import net.infoxication.reactstarprnt.RNStarPrntPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.horcrux.svg.SvgPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNStarPrntPackage(),
             new FastImageViewPackage(),
             new SvgPackage(),
             new RNCWebViewPackage(),
