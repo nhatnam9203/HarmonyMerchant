66b71d67b81c45d2fa5f016f0660d374740db29b
Nguyễn Hoàng Nhật Phi <nguyenhoangnhatphi@gmail.com>
2019-08-01 12:09:49 +0700

    setup rn-fetch-blob for android

--- android/app/src/main/java/com/hpmerchant/MainApplication.java
+++ android/app/src/main/java/com/hpmerchant/MainApplication.java
@@ -3,6 +3,7 @@ package com.hpmerchant;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import net.infoxication.reactstarprnt.RNStarPrntPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.horcrux.svg.SvgPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new RNStarPrntPackage(),
             new FastImageViewPackage(),
             new SvgPackage(),
