2709c2155c4f11ec93e7da3e589758f46ad27d15
Nguyễn Hoàng Nhật Phi <nguyenhoangnhatphi@gmail.com>
2019-08-07 18:11:25 +0700

    change end point production

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,6 +1,6 @@
 const apiConfigs = {
-    BASE_API: "http://api2.levincidemo.com/api/",
-    // BASE_API: "http://hpapiversion2.azurewebsites.net/api/",
+    // BASE_API: "http://api2.levincidemo.com/api/",
+    BASE_API: "http://api.harmonypayment.com/api/",
 }
 
 export default apiConfigs;
