b86e26e3d8a5c96f78a571fbb59569c7fab24953
Tu Tran <tu.tran@levincigroup.com>
2019-08-12 14:42:57 +0700

    setup macbook's company

--- src/screens/HomeScreen/widget/TabMarketing/widget/TabPromotion/index.js
+++ src/screens/HomeScreen/widget/TabMarketing/widget/TabPromotion/index.js
@@ -1,4 +1,4 @@
-import {Alert,NativeModules} from 'react-native';
+import { Alert, NativeModules } from 'react-native';
 
 import Layout from './layout';
 import connectRedux from '@redux/ConnectRedux';
@@ -8,64 +8,64 @@ const PosLink = NativeModules.MyApp;
 
 class TabPromotion extends Layout {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            name:'Phi'
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Phi'
     }
+  }
 
-    applyPromorion =() =>{
-        PosLink.setupPax('192.168.0.112','10009','60000',message => alert(message));
-    }
+  applyPromorion = () => {
+    PosLink.setupPax('192.168.0.112', '10009', '60000', message => alert(message));
+  }
 
-    paymentCredit = () =>{
-        Alert.alert(
-            `Alert Title`,
-            `Transaction is processing,please wait...`,
-            [
-              {
-                text: 'Cancel',
-                onPress: () => PosLink.cancelTransaction(),
-                style: 'cancel',
-              },
-            ],
-            {cancelable: false},
-          );
-        PosLink.sendTransaction(this.callback)
-    }
+  paymentCredit = () => {
+    Alert.alert(
+      `Alert Title`,
+      `Transaction is processing,please wait...`,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => PosLink.cancelTransaction(),
+          style: 'cancel',
+        },
+      ],
+      { cancelable: false },
+    );
+    PosLink.sendTransaction(this.callback)
+  }
 
-    callback(message){
-        alert(message)
-    }
-    
-    callback1(message){
-        alert(message)
-    }
-    
+  callback(message) {
+    alert(message)
+  }
 
-    paymentCredit1 =() =>{
-        PosLink.sendTransaction(message => {
-            Alert.alert(
-                `Alert Title`,
-                `${message}`,
-                [
-                  {
-                    text: 'Cancel',
-                    onPress: () => PosLink.cancelTransaction(),
-                    style: 'cancel',
-                  },
-                ],
-                {cancelable: false},
-              );
-        }, messageEnd => alert(messageEnd));
-    }
+  callback1(message) {
+    alert(message)
+  }
+
+
+  paymentCredit1 = () => {
+    PosLink.sendTransaction(message => {
+      Alert.alert(
+        `Alert Title`,
+        `${message}`,
+        [
+          {
+            text: 'Cancel',
+            onPress: () => PosLink.cancelTransaction(),
+            style: 'cancel',
+          },
+        ],
+        { cancelable: false },
+      );
+    }, messageEnd => alert(messageEnd));
+  }
 
 }
 
 const mapStateToProps = state => ({
-    profile: state.dataLocal.profile,
-    language: state.dataLocal.language
+  profile: state.dataLocal.profile,
+  language: state.dataLocal.language
 })
 
 
