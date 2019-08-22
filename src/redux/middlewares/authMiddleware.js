4ac9d21cc640b1976cfef8e0d65a98a8d0b5075e
phinhn <nguyenhoangnhatphi@gmail.com>
2019-08-22 15:21:41 +0700

    use token of staff

--- src/redux/middlewares/authMiddleware.js
+++ src/redux/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 const authMiddleware = store => next => action => {
     if (action.token) {
         const appState = store.getState();
-        return next({ ...action, token: appState.dataLocal.token });
+        return next({ ...action, token: appState.dataLocal.profileStaffLogin.token });
 
     }
     return next(action);
