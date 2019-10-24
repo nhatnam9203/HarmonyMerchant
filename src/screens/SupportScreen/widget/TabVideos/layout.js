a2a50c26f1a80a1ba2ec68fc208eaec8ce175c95
phinhn <nguyenhoangnhatphi@gmail.com>
2019-10-24 12:27:37 +0700

    UI tab videos

--- src/screens/SupportScreen/widget/TabVideos/layout.js
+++ src/screens/SupportScreen/widget/TabVideos/layout.js
@@ -19,17 +19,25 @@ class Layout extends React.Component {
     render() {
         return (
             <View style={styles.container} >
-                <YouTube
-                    videoId="eQq5knMITLk"
-                    play
-                    fullscreen
-                    loop
-                    onReady={e => this.setState({ isReady: true })}
-                    onChangeState={e => this.setState({ status: e.state })}
-                    onChangeQuality={e => this.setState({ quality: e.quality })}
-                    onError={e => this.setState({ error: e.error })}
-                    style={{ alignSelf: 'stretch', height: 300 }}
-                />
+                <View style={{width:scaleSzie(240)}} >
+                    <YouTube
+                        videoId="eQq5knMITLk"
+                        play={false}
+                        fullscreen={true}
+                        loop
+                        onReady={e => this.setState({ isReady: true })}
+                        onChangeState={e => this.setState({ status: e.state })}
+                        onChangeQuality={e => this.setState({ quality: e.quality })}
+                        onError={e => this.setState({ error: e.error })}
+                        style={{ width:scaleSzie(240),height:scaleSzie(180)}}
+                    />
+                    <View style={{paddingRight:scaleSzie(10),marginTop:scaleSzie(10)}} >
+                        <Text>
+                        HarmonyPayment - Mobile Payment App With POS Systems
+                        </Text>
+                    </View>
+                </View>
+
             </View>
 
         );
