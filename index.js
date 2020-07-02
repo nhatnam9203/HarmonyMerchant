141ca3f8562d173a858118a8f2cf3a5f6764b924
phinhn <nguyenhoangnhatphi@gmail.com>
2020-07-02 12:51:44 +0700

    fix redux-saga

--- index.js
+++ index.js
@@ -5,29 +5,29 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 
 import AppNavigators from './src/navigators/AppNavigator';
-import configureStore from './src/redux/store';
+// import configureStore from './src/redux/store';
+import { persistor,store} from './src/redux/store';
 import { name as appName } from './app.json';
 import NavigatorServices from './src/navigators/NavigatorServices';
 import { Loading, PopupDisconnected, PopupConnected } from './src/components';
-import IMAGE from './src/resources';
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        const { persistor, store } = configureStore();
-        this.state = {
-            persistor,
-            store,
-        }
+        // const { persistor, store } = configureStore();
+        // this.state = {
+        //     persistor,
+        //     store,
+        // }
     }
 
     render() {
         return (
-            <Provider store={this.state.store}>
+            <Provider store={store}>
                 <PersistGate
                     loading={<View />}
-                    persistor={this.state.persistor}>
+                    persistor={persistor}>
                         <AppNavigators
                             ref={navigatorRef => {
                                 NavigatorServices.setContainer(navigatorRef);
