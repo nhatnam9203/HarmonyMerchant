0e12ec6bb432761927927a517886cbe5c56f979f
phinhn <nguyenhoangnhatphi@gmail.com>
2020-12-01 17:43:18 +0700

    update code

--- src/screens/CustomerScreen/widget/EditOrCreateCustomerTab.js
+++ src/screens/CustomerScreen/widget/EditOrCreateCustomerTab.js
@@ -4,10 +4,11 @@ import {
     StyleSheet,
     Platform,
     Image,
-    ScrollView
+    ScrollView,
+    TextInput
 } from 'react-native';
 
-import { Button, Text } from '@components';
+import { Button, Text, Dropdown } from '@components';
 import { scaleSzie } from '@utils';
 import Configs from "@configs";
 import ICON from "@resources";
@@ -41,20 +42,148 @@ class EditOrCreateCustomerTab extends React.Component {
                 </View>
 
                 {/* ------------- Line ------------ */}
-                <View style={{ height: 1, backgroundColor: "#EEEEEE", marginBottom: scaleSzie(18) }} />
+                <View style={{ height: 1, backgroundColor: "#EEEEEE" }} />
 
                 <View style={{ flex: 1, flexDirection: "row" }} >
                     {/* ------------------ Left Content ------------------ */}
-                    <View style={{ flex: 1}} >
+                    <View style={{ flex: 1 }} >
+                        <ScrollView>
+                            <View style={{ height: scaleSzie(18) }} />
+                            {/* --------- Item -------- */}
+                            <FromItem
+                                title={`First Name`}
+                                isRequired={true}
+                                placeholder="First name"
+                            />
 
+                            <FromItem
+                                title={`Last Name`}
+                                isRequired={true}
+                                placeholder="Last Name"
+                            />
+
+                            <FromItem
+                                title={`Phone Number`}
+                                isRequired={true}
+                                placeholder="Enter phone number"
+                            />
+
+                            <FromItem
+                                title={`Contact email`}
+                                placeholder="Email address"
+                            />
+
+                            <Text style={{ fontSize: scaleSzie(14), color: "#404040", fontWeight: "600" }} >
+                                {`Gender`}
+                            </Text>
+                            <View style={{ width: scaleSzie(150), height: scaleSzie(35), marginTop: scaleSzie(10) }} >
+                                <Dropdown
+                                    label={"Gender"}
+                                    data={[
+                                        { value: "Male" },
+                                        { value: "Female" }
+                                    ]}
+                                    value={"timezone"}
+                                    // onChangeText={(timezone) => this.setState({ timezone })}
+                                    containerStyle={{
+                                        borderWidth: 1,
+                                        borderColor: '#C5C5C5',
+                                        flex: 1
+                                    }}
+                                />
+                            </View>
+
+                        </ScrollView>
                     </View>
 
                     {/* ----------------- Padding ------------ */}
-                    <View style={{width:scaleSzie(30)}} />
+                    <View style={{ width: scaleSzie(30) }} />
 
                     {/* ------------------ Right Content ------------------ */}
                     <View style={{ flex: 1 }} >
+                        <ScrollView>
+                            <View style={{ height: scaleSzie(18) }} />
+                            <Text style={{ fontSize: scaleSzie(14), color: "#404040", fontWeight: "600" }} >
+                                {`Birthday`}
+                            </Text>
 
+                            <View style={{ height: scaleSzie(35), flexDirection: "row", justifyContent: "space-between", marginTop: scaleSzie(10),marginBottom:scaleSzie(14)}} >
+
+                                <View style={{ width: scaleSzie(95), }} >
+                                    <Dropdown
+                                        label={"Day"}
+                                        data={[
+                                            { value: "Male" },
+                                            { value: "Female" }
+                                        ]}
+                                        value={""}
+                                        // onChangeText={(timezone) => this.setState({ timezone })}
+                                        containerStyle={{
+                                            borderWidth: 1,
+                                            borderColor: '#C5C5C5',
+                                            flex: 1
+                                        }}
+                                    />
+                                </View>
+                                <View style={{ width: scaleSzie(95), }} >
+                                    <Dropdown
+                                        label={"Month"}
+                                        data={[
+                                            { value: "Male" },
+                                            { value: "Female" }
+                                        ]}
+                                        value={""}
+                                        // onChangeText={(timezone) => this.setState({ timezone })}
+                                        containerStyle={{
+                                            borderWidth: 1,
+                                            borderColor: '#C5C5C5',
+                                            flex: 1
+                                        }}
+                                    />
+                                </View>
+                                <View style={{ width: scaleSzie(95), }} >
+                                    <Dropdown
+                                        label={"Year"}
+                                        data={[
+                                            { value: "Male" },
+                                            { value: "Female" }
+                                        ]}
+                                        value={""}
+                                        // onChangeText={(timezone) => this.setState({ timezone })}
+                                        containerStyle={{
+                                            borderWidth: 1,
+                                            borderColor: '#C5C5C5',
+                                            flex: 1
+                                        }}
+                                    />
+                                </View>
+                            </View>
+
+                             {/* ------------ Address --------- */}
+                             <FromItem
+                                title={`Address`}
+                                isRequired={true}
+                                placeholder="Address"
+                            />
+
+                            {/* ----------- City + State ------------- */}
+
+                             {/* ------------ Referrer Phone Number --------- */}
+                             <FromItem
+                                title={`Referrer Phone Number`}
+                                isRequired={true}
+                                placeholder="Enter phone number"
+                            />
+
+                             {/* ------------ Note --------- */}
+                             <Text style={{ fontSize: scaleSzie(14), color: "#404040", fontWeight: "600" ,marginBottom:scaleSzie(10)}} >
+                                {`Note:`}
+                            </Text>
+                            <View style={{height:scaleSzie(60),borderColor:"#CCCCCC",borderWidth:1}} >
+
+                            </View>
+
+                        </ScrollView>
                     </View>
                 </View>
             </View>
@@ -62,6 +191,34 @@ class EditOrCreateCustomerTab extends React.Component {
     }
 }
 
+const FromItem = ({ title, isRequired, placeholder }) => {
+
+    return (
+        <View style={{ marginBottom: scaleSzie(14) }} >
+            <Text style={{ fontSize: scaleSzie(14), color: "#404040", fontWeight: "600" }} >
+                {`${title}`}
+                {
+                    isRequired ? <Text style={{ color: "#FF3B30" }} >
+                        {` *`}
+                    </Text> : null
+                }
+
+            </Text>
+
+            <View style={{ height: scaleSzie(35), borderWidth: 1, borderColor: "#CCCCCC", marginTop: scaleSzie(10), paddingHorizontal: scaleSzie(10) }} >
+                <TextInput
+                    style={{
+                        flex: 1,
+                        fontSize: scaleSzie(14),
+                        color: "#404040"
+
+                    }}
+                    placeholder={placeholder ? placeholder : ""}
+                />
+            </View>
+        </View>
+    );
+}
 
 
 const styles = StyleSheet.create({
