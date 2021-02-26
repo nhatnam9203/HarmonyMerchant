f6a5f6ff86e93f874e3d3dee8f7b2e22555dd603
phinhn <nguyenhoangnhatphi@gmail.com>
2021-02-26 14:34:20 +0700

    update code

--- src/navigators/DrawerStack.js
+++ src/navigators/DrawerStack.js
@@ -26,7 +26,7 @@ const DrawerStack = createDrawerNavigator(
     Reports: ReportScreen,
     GiftCard: GiftCardScreen
   }, {
-  initialRouteName: "Home",
+  initialRouteName: "Customer",
   contentComponent: SlideDrawer,
   drawerWidth: scaleSzie(220),
   overlayColor: "rgba(0, 0, 0, 0.5) ",
