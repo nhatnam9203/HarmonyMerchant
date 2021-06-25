397e1a9660d873a4a5e84a3d9b087a7b4ca10d1c
nam <nam.phan@levincigroup.com>
2021-06-25 11:23:04 +0700

    key board

--- src/components/PopupCheckStaffPermission.js
+++ src/components/PopupCheckStaffPermission.js
@@ -1,166 +1,193 @@
 import React from 'react';
-import {
-    View,
-    Text,
-    Keyboard,
-    ActivityIndicator,
-    Alert
-} from 'react-native';
+import { View, Text, ActivityIndicator, Alert } from 'react-native';
 import { TextInputMask } from 'react-native-masked-text';
 
 import ButtonCustom from './ButtonCustom';
 import PopupParent from './PopupParent';
 import { scaleSzie } from '../utils';
+import KeyboardNumeric from "./KeyboardNumeric";
 import connectRedux from '@redux/ConnectRedux';
 
 class PopupCheckStaffPermission extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      customStyle: {},
+      appointmentId: '',
+      isBlock: false,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            customStyle: {},
-            appointmentId: "",
-            isBlock: false
-        }
+  setStateFromParent = async (
+    value = '',
+    appointmentId = '',
+    isBlock = false,
+  ) => {
+    await this.setState({
+      value,
+      appointmentId,
+      isBlock,
+    });
+  };
+
+  onChangeValue = (number) => {
+    if (number === ".") return;
+    let { value } = this.state;
+    if (number === "x") {
+      value = value.substring(0, value.length - 1);
+    } else {
+      if (value.toString().length < 4) {
+        value += number;
+      }
     }
+    this.setState({ value })
+  }
 
-    componentDidMount() {
-        this.keyboardDidShowListener = Keyboard.addListener('keyboardWillShow', this.keyboardDidShow);
-        this.keyboardDidHideListener = Keyboard.addListener('keyboardWillHide', this.keyboardDidHide);
+  submitPin = () => {
+    const { profile, tabName } = this.props;
+    const { value, appointmentId, isBlock } = this.state;
+    if (value.length === 4) {
+      this.props.actions.auth.checkStaffPermission(
+        profile.merchantCode,
+        value,
+        tabName,
+        appointmentId,
+        isBlock,
+      );
+    } else {
+      Alert.alert(`PIN must be 4 digits.`);
     }
+  };
 
-    setStateFromParent = async (value = "", appointmentId = "", isBlock = false) => {
-        await this.setState({
-            value,
-            appointmentId,
-            isBlock
-        })
-    }
+  onRequestClose = async () => {
+    await this.setState({
+      value: '',
+    });
+    this.props.onRequestClose();
+  };
 
-    keyboardDidShow = async () => {
-        await this.setState({
-            customStyle: {
-                justifyContent: 'flex-start',
-                paddingTop: scaleSzie(50)
-            }
-        });
-    }
-
-    keyboardDidHide = async () => {
-        await this.setState({
-            customStyle: {}
-        });
-
-    }
-
-    submitPin = () => {
-        const { profile, tabName } = this.props;
-        const { value, appointmentId, isBlock } = this.state;
-        if (value.length === 4) {
-            this.props.actions.auth.checkStaffPermission(profile.merchantCode, value, tabName, appointmentId, isBlock);
-        } else {
-            Alert.alert(`PIN must be 4 digits.`);
-        }
-    }
-
-    onRequestClose = async () => {
-        await this.setState({
-            value: ""
-        });
-        this.props.onRequestClose();
-    }
-
-    render() {
-        const { title, isLoadingCheckStaffPermission,
-            visiblePopupCheckStaffPermission, hideCloseButton, tabName
-        } = this.props;
-        const { value, customStyle, appointmentId } = this.state;
-        return (
-            <PopupParent
-                title={`${title}`}
-                visible={visiblePopupCheckStaffPermission}
-                onRequestClose={this.onRequestClose}
-                hideCloseButton={hideCloseButton}
-                style={customStyle}
+  render() {
+    const {
+      title,
+      isLoadingCheckStaffPermission,
+      visiblePopupCheckStaffPermission,
+      hideCloseButton,
+      tabName,
+    } = this.props;
+    const { value, customStyle, appointmentId } = this.state;
+    return (
+      <PopupParent
+        title={`${title}`}
+        visible={visiblePopupCheckStaffPermission}
+        onRequestClose={this.onRequestClose}
+        hideCloseButton={hideCloseButton}
+        style={customStyle}
+      >
+        <View
+          style={{
+            minHeight: scaleSzie(460),
+            maxHeight: scaleSzie(530),
+            backgroundColor: '#fff',
+            borderBottomLeftRadius: scaleSzie(15),
+            borderBottomRightRadius: scaleSzie(15),
+          }}
+        >
+          <View
+            style={{
+              alignItems: 'center',
+              marginTop: scaleSzie(18),
+              marginBottom: scaleSzie(4),
+            }}
+          >
+            <Text style={{ color: '#404040', fontSize: scaleSzie(18), fontWeight: '500', marginTop: scaleSzie(5) }}>
+              {'Please enter the authorized PIN number'}
+            </Text>
+          </View>
+          <View
+            style={{ flex: 1, marginVertical: scaleSzie(15), alignItems: 'center' }}
+          >
+            <View
+              style={{
+                width: '90%',
+                height: scaleSzie(45),
+                borderColor: '#dddddd',
+                borderWidth: 2,
+              }}
             >
-                <View style={{
-                    height: scaleSzie(150), backgroundColor: '#fff',
-                    borderBottomLeftRadius: scaleSzie(15), borderBottomRightRadius: scaleSzie(15)
-                }} >
-                    <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: scaleSzie(10), marginBottom: scaleSzie(4) }} >
-                        <Text style={{ color: '#404040', fontSize: scaleSzie(18) }} >
-                            {'Please enter the authorized PIN number'}
-                        </Text>
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
-                        <View style={{
-                            width: '90%', height: scaleSzie(45),
-                            borderColor: 'rgb(231,231,231)', borderWidth: 3
-                        }} >
-                            <TextInputMask
-                                type="only-numbers"
-                                style={{
-                                    flex: 1, fontSize: scaleSzie(18), textAlign: 'center',
-                                    padding: 0, margin: 0
-                                }}
-                                placeholder="Your PIN"
-                                keyboardType="numeric"
-                                maxLength={4}
-                                value={value}
-                                onChangeText={(value) => this.setState({ value })}
-                                onSubmitEditing={this.submitPin}
-                                secureTextEntry={true}
-                            />
-                        </View>
-                    </View>
-                    <View style={{
-                        height: scaleSzie(45), alignItems: 'center'
-                    }} >
-                        {
-                            isLoadingCheckStaffPermission ? <View style={{
-                                width: '30%', height: scaleSzie(35), backgroundColor: '#0764B0',
-                                justifyContent: 'center', alignItems: 'center'
-                            }} >
-                                <ActivityIndicator
-                                    size="large"
-                                    color="#fff"
-                                />
-                            </View> : <ButtonCustom
-                                    width={'30%'}
-                                    height={35}
-                                    backgroundColor="#0764B0"
-                                    title="SUBMIT"
-                                    textColor="#fff"
-                                    onPress={this.submitPin}
-                                    styleText={{
-                                        fontSize: scaleSzie(14)
-                                    }}
-                                    style={{
-                                        borderRadius: scaleSzie(4)
-                                    }}
-                                />
-                        }
-                    </View>
-                </View>
-            </PopupParent>
-        );
-    }
+              <TextInputMask
+                type="only-numbers"
+                style={{
+                  flex: 1,
+                  fontSize: scaleSzie(18),
+                  textAlign: 'center',
+                  padding: 0,
+                  margin: 0,
+                }}
+                placeholder="Your PIN"
+                keyboardType="numeric"
+                maxLength={4}
+                value={value}
+                onChangeText={(value) => this.setState({ value })}
+                onSubmitEditing={this.submitPin}
+                secureTextEntry={true}
+                editable={false}
+                showSoftInputOnFocus={false}
+              />
+            </View>
+            <KeyboardNumeric onPress={this.onChangeValue} />
+          </View>
+          <View
+            style={{
+              height: scaleSzie(45),
+              alignItems: 'center',
+              marginBottom : scaleSzie(15)
+            }}
+          >
+            {isLoadingCheckStaffPermission ? (
+              <View
+                style={{
+                  width: '35%',
+                  height: scaleSzie(40),
+                  backgroundColor: '#0764B0',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <ActivityIndicator size="large" color="#fff" />
+              </View>
+            ) : (
+                <ButtonCustom
+                  width={'35%'}
+                  height={40}
+                  backgroundColor="#0764B0"
+                  title="SUBMIT"
+                  textColor="#fff"
+                  onPress={this.submitPin}
+                  styleText={{
+                    fontSize: scaleSzie(14),
+                  }}
+                  style={{
+                    borderRadius: scaleSzie(4),
+                  }}
+                />
+              )}
+          </View>
+        </View>
+      </PopupParent>
+    );
+  }
 
-
-
-    componentWillUnmount() {
-        this.keyboardDidShowListener?.remove();
-        this.keyboardDidHideListener?.remove();
-    }
-
+  componentWillUnmount() {
+    this.keyboardDidShowListener?.remove();
+    this.keyboardDidHideListener?.remove();
+  }
 }
 
-
-const mapStateToProps = state => ({
-    language: state.dataLocal.language,
-    isLoadingCheckStaffPermission: state.auth.isLoadingCheckStaffPermission,
-    profile: state.dataLocal.profile
+const mapStateToProps = (state) => ({
+  language: state.dataLocal.language,
+  isLoadingCheckStaffPermission: state.auth.isLoadingCheckStaffPermission,
+  profile: state.dataLocal.profile,
 });
 
 export default connectRedux(mapStateToProps, PopupCheckStaffPermission);
