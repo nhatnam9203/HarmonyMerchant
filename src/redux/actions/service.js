3bdcb2dd9025da5956cecf100fb37009b8a358a7
hangkimduyen <hangkimduyen@gmail.com>
2021-07-07 16:15:05 +0700

    change apiConfigs

--- src/redux/actions/service.js
+++ src/redux/actions/service.js
@@ -93,7 +93,7 @@ export function getServiceByStaff(categoryId, staffId, callBack) {
         type: 'GET_SERVICE_BY_STAFF',
         method: 'GET',
         token: true,
-        api: `${apiConfigs.BASE_API}service/getbycategory/${categoryId}?staffIf=${staffId}`,
+        api: `service/getbycategory/${categoryId}?staffIf=${staffId}`,
         callBack
     }
 }
