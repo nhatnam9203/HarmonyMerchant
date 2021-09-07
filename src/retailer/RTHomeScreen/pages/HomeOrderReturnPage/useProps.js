237a96ef3dbb1f590482bae267f4354d8f2963e4
Trần Hoàng Nhã <thediffest@gmail.com>
2021-09-07 17:37:46 +0700

    fix return amount toFixed

--- src/retailer/RTHomeScreen/pages/HomeOrderReturnPage/useProps.js
+++ src/retailer/RTHomeScreen/pages/HomeOrderReturnPage/useProps.js
@@ -72,8 +72,9 @@ export const useProps = ({ params: { item } }) => {
         _.get(updateItem, "bookingProductId") ==
         _.get(itemQuantity, "bookingProductId")
       ) {
-        tempItem.returnAmount =
-          (originItem.total / originItem.quantity) * value;
+        tempItem.returnAmount = parseFloat(
+          (originItem.total / originItem.quantity) * value
+        ).toFixed(2);
         tempItem.returnQuantity = value;
       }
       return tempItem;
