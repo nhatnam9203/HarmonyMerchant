2aeebfff1858a369b02b74ae3542c8e23240209b
Trần Hoàng Nhã <thediffest@gmail.com>
2021-10-05 17:40:25 +0700

    enable giftcard

--- src/retailer/RTHomeScreen/pages/HomeOrderPayPage/Layout.js
+++ src/retailer/RTHomeScreen/pages/HomeOrderPayPage/Layout.js
@@ -243,12 +243,12 @@ export const Layout = ({
                 />
               </View>
               <View style={styles.rowContent}>
-                {/* <ButtonPaymentMethod
+                <ButtonPaymentMethod
                   key={"Gift Card"}
                   title={"Gift Card"}
                   selectedPayment={selectedPayment}
                   paymentSelected={paymentSelected}
-                /> */}
+                />
               </View>
             </View>
 
