64c296ef5b1f515deeadf7fe33ddae86427a05f6
Trần Hoàng Nhã <thediffest@gmail.com>
2021-11-08 14:13:47 +0700

    retailer report

--- src/navigators/RetailerNavigator.js
+++ src/navigators/RetailerNavigator.js
@@ -11,13 +11,14 @@ import {
 import { RTCustomerScreen } from "@src/retailer/RTCustomerScreen";
 import { RTHomeScreen } from "@src/retailer/RTHomeScreen";
 import { RTInventoryScreen } from "@src/retailer/RTInventoryScreen";
-import { RTReportsScreen } from "@src/retailer/RTReportsScreen";
+// import { RTReportsScreen } from "@src/retailer/RTReportsScreen";
 import { RTSettingsScreen } from "@src/retailer/RTSettingsScreen";
 import { RTStaffCheckIn } from "@src/retailer/RTStaffCheckIn";
 import React from "react";
 import { SettlementScreen, SupportScreen, InvoiceScreen } from "../screens";
 import actions from "@actions";
 import { useSelector, useDispatch } from "react-redux";
+import { ReportScreen } from "../merchant/ReportScreen";
 
 const { Screen, Navigator } = createDrawerNavigator();
 
@@ -67,7 +68,7 @@ export const RetailerNavigator = () => {
       />
       <Screen {...RTCustomerScreen} />
       <Screen {...RTInventoryScreen} />
-      <Screen {...RTReportsScreen} />
+      <Screen {...ReportScreen} />
       <Screen {...RTStaffCheckIn} />
       <Screen {...RTSettingsScreen} />
       {/* <Screen {...RTSupportScreen} /> */}
