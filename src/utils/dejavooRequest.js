2ed0a85fc024886093172908dc850eee83a34511
hangkimduyen <hangkimduyen@gmail.com>
2022-02-09 13:19:28 +0700

    increase timeout for dejavoo

--- src/utils/dejavooRequest.js
+++ src/utils/dejavooRequest.js
@@ -32,7 +32,7 @@ export const requestTransactionDejavoo = async (params) => {
     baseURL: api,
     url: `?TerminalTransaction=${param}`,
     headers: headers,
-    timeout: 300000,
+    timeout: 420000,
     };
     const response = await handleRequest(configs)
     return response
