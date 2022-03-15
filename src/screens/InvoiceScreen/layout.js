49dae292bc727913c5886ebfb1a7a93d4d092f4b
Duyen Hang <duyen.hang@harmonypayment.com>
2022-03-15 10:57:51 +0700

    fix bug merge code

--- src/screens/InvoiceScreen/layout.js
+++ src/screens/InvoiceScreen/layout.js
@@ -750,12 +750,6 @@ export default class Layout extends React.Component {
                       {data.paymentMethod === "credit_card" ||
                       data.paymentMethod === "debit_card" ? (
                         <View style={{ marginTop: scaleSize(5) }}>
-                          {data?.fee > 0 && (
-                            <ItemTotal
-                              title={" Non-Cash Adjustment"}
-                              value={data?.fee}
-                            />
-                          )}
                           <Text style={[layouts.fontPrintStyle]}>
                             {` ${
                               data?.paymentInformation?.type || ""
@@ -802,22 +796,7 @@ export default class Layout extends React.Component {
                             }`}
                           </Text>
                         </View>
-                      ) : (
-                        <>
-                          {data?.fee > 0 && (
-                            <ItemTotal
-                              title={" Non-Cash Adjustment"}
-                              value={data?.fee}
-                            />
-                          )}
-                          {data?.cashDiscount < 0 && (
-                            <ItemTotal
-                              title={" Cash Discount"}
-                              value={data?.cashDiscount}
-                            />
-                          )}
-                        </>
-                      )}
+                      ) : null}
                     </View>
                   ))}
                 </View>
