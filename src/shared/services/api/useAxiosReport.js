7e1bf5a73c11beb35f52695c107727d3ec0feafb
Duyen Hang <duyen.hang@harmonypayment.com>
2022-03-23 14:02:37 +0700

    change

--- src/shared/services/api/useAxiosReport.js
+++ src/shared/services/api/useAxiosReport.js
@@ -32,6 +32,48 @@ axios.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+axios.interceptors.response.use(
+  (response) => {
+    const { codeStatus = 0, codeNumber = 0, message } = response?.data;
+    switch (parseInt(codeNumber, 10)) {
+      case 401:
+        if (parseInt(codeStatus) === 5) {
+          alert("Permission Denied");
+        } else {
+          // NavigationServices.logout();
+        }
+        break;
+      case 404: // not found
+        break;
+
+      case 400: // thieu field
+        if (codeStatus !== 2 && codeStatus !== 4) {
+          setTimeout(() => {
+            alert(`${message}`);
+          }, 100);
+        }
+
+        break;
+      default:
+        break;
+    }
+
+    return response;
+  },
+  async (error) => {
+    const config = error?.config;
+
+    if (error?.response?.status === 401 && !config._retry) {
+      config._retry = true;
+      // localStorage.setItem('token', await refreshAccessToken());
+
+      return axios(config);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export const useAxiosReport = makeUseAxios({
   axios: axios,
 });
