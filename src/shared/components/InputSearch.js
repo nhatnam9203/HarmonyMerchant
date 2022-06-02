69450b12bbc29734c39b73bef9fe6f3a87300f05
Duyen Hang <duyen.hang@harmonypayment.com>
2022-06-02 12:19:09 +0700

    increase timeout for input search

--- src/shared/components/InputSearch.js
+++ src/shared/components/InputSearch.js
@@ -14,7 +14,7 @@ export const InputSearch = React.forwardRef(
         if (onSearch && typeof onSearch === "function") {
           onSearch(_prop);
         }
-      }, 800),
+      }, 2000),
       [onSearch]
     );
 
